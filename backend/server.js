61f09d9dcbf303b3d9464e288fa18b5a1225b886
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-12 18:29:10 -0800

    Connect backend with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const data = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import data from './data/products.js'
 
 const app = express()
 
