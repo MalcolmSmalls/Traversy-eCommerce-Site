ee8dadc50f0018e61932d96f5197235baf06345a
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-23 13:36:21 -0800

    Add to cart now adds product by id to cart state and also saves to localSTorage

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useParams, useNavigate, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
 export default function CartScreen() {
+  const productId = useParams()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
   return <div>CartScreen</div>
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,7 +8,7 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 
 export default function ProductScreen() {
-  const [qty, setQty] = useState(0)
+  const [qty, setQty] = useState(1)
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const productDetails = useSelector((state) => state.productDetails)
