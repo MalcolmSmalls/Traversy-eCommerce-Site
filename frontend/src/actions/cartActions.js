5491d4d0367d1e541d624651e6b28ade5d6ece14
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-26 11:22:41 -0800

    Finish cart page and allow quantity to be selected from it

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -6,7 +6,6 @@ export const addToCart =
   ({ id }, qty) =>
   async (dispatch, getState) => {
     const { data } = await axios.get(`/api/products/${id}`)
-
     dispatch({
       type: CART_ADD_ITEM,
       payload: {
