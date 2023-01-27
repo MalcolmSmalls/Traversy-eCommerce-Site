0248f0c5012278c3b7d506beb37f189c2f052493
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-26 16:55:28 -0800

    Add authentication that checks if email and password match database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDB from "./config/db.js"
+import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
-
+import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import bodyParser from 'body-parser'
 
 const app = express()
 
@@ -11,14 +12,21 @@ dotenv.config()
 connectDB()
 
 app.get('/', (req, res) => {
-    res.send('API is working')
+  res.send('API is working')
 })
-
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
+app.use(express.json())
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Now running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Now running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
